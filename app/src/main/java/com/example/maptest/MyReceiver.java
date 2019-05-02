e595e42205506362db23e75e15d4507f91e04bc0
karimhekal <karimhekal@gmail.com>
2019-05-02 12:27:40 +0200

    third commit

---
+++ app/src/main/java/com/example/maptest/MyReceiver.java
@@ -0,0 +1,25 @@
+package com.example.maptest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+
+        int datapassed = intent.getIntExtra("DATAPASSED", 0);
+        String orgData = intent.getStringExtra("DATA_BACK");
+
+        Toast.makeText(context,
+                "Triggered by Service!\n"
+                        + "Data passed: " + String.valueOf(datapassed) + "\n"
+                        + "original Data: " + orgData,
+                Toast.LENGTH_LONG).show();
+
+    }
+}
