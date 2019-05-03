ae2133afaf8e81264b827170cb3c81206f816b9a
karimhekal <karimhekal@gmail.com>
2019-05-03 09:29:13 +0200

    third commit

--- app/src/main/java/com/example/maptest/MyService.java
+++ app/src/main/java/com/example/maptest/MyService.java
@@ -77,28 +77,6 @@ public class MyService extends Service implements OnMapReadyCallback, LocationLi
 
     boolean shown=false;
 
-    private void showNotification(String title,String description) {
-        String CHANNEL_ID = "my_channel_01";
-
-        // Create an explicit intent for an Activity in your app
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(title)
-                .setContentText(description)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-// notificationId is a unique int for each notification that you must define
-        notificationManager.notify(0, builder.build());
-    }
 
     @Override
     public void onDestroy() {
@@ -444,6 +422,28 @@ public class MyService extends Service implements OnMapReadyCallback, LocationLi
                     Toast.makeText(MyService.this, "OUTSIDEEE", Toast.LENGTH_SHORT).show();
                 }
             }}
+    }
+    private void showNotification(String title,String description) {
+        String CHANNEL_ID = "my_channel_01";
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(title)
+                .setContentText(description)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(0, builder.build());
     }
 
     GoogleApiClient mGoogleApiClient;
